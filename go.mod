35c2737c9fb03be004e4e389505704ce48d87405
liangbo <liangbo01@cpirhzl.com>
2024-07-23 22:18:36 +0800

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module dscserver
+
+go 1.16
+
+require (
+	github.com/liangboceo/dependencyinjection v1.0.0
+	github.com/liangboceo/yuanboot v1.9.0
+)
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"dscserver/controller"
+	"github.com/liangboceo/dependencyinjection"
+	"github.com/liangboceo/yuanboot/abstractions"
+	nacosconfig "github.com/liangboceo/yuanboot/pkg/configuration/nacos"
+	"github.com/liangboceo/yuanboot/pkg/servicediscovery/nacos"
+	"github.com/liangboceo/yuanboot/web"
+	"github.com/liangboceo/yuanboot/web/actionresult/extension"
+	"github.com/liangboceo/yuanboot/web/mvc"
+)
+
+func main() {
+	CreateMVCBuilder().Build().Run()
+}
+
+// * Create the builder of Web host
+func CreateMVCBuilder() *abstractions.HostBuilder {
+	configuration := nacosconfig.RemoteConfig("bootstrap")
+	return web.NewWebHostBuilder().
+		UseConfiguration(configuration).
+		Configure(func(app *web.ApplicationBuilder) {
+			app.SetJsonSerializer(extension.CamelJson())
+			app.UseMvc(func(builder *mvc.ControllerBuilder) {
+				builder.AddViewsByConfig()                          //视图
+				builder.AddController(controller.NewDemoController) // 注册mvc controller
+			})
+		}).
+		ConfigureServices(func(serviceCollection *dependencyinjection.ServiceCollection) {
+			// ioc
+			nacos.UseServiceDiscovery(serviceCollection)
+		})
+}
