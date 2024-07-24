536ba2560d68823590174e3420c0a129bc914b3e
liangbo <liangbo01@cpirhzl.com>
2024-07-24 17:53:32 +0800

    fix:增加缓存service

--- build.sh
+++ build.sh
@@ -1,31 +1,28 @@
 #!/usr/bin/env bash
-osArray=("linux" "darwin" "freebsd" "windows")
-archs=("amd64" "386")
-version=${1-"0.0.1-preview1"}
+version=${1-"1.0.0-SNAPSHOT"}
 out_file="dscServer"
 
 build() {
-  os=$1
-  arch=$2
+  local os="$1"
+  local arch="$2"
+  local dir="build/dscServer-$os-$arch"
+  out_file="${out_file}-${version}"
   go env -w GOPROXY=https://goproxy.cn,direct
-  GOOS=$os GOARCH=$arch go build -o "${out_file}" .
-  tgzName="${out_file}_${version}_${os}_${arch}.tar.gz"
-  rm -rf build
-  mkdir build
-  tar -czf "build/${tgzName}" "${out_file}"
-  rm -f "${out_file}"
+  [ "$os" = "windows" ] && {
+  		out_file="${out_file}.exe"
+  	}
+  rm -rf $dir
+  mkdir -p $dir
+  cp bootstrap*.yml $dir
+  GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -o "${dir}/${out_file}" .
+
 }
 
 main() {
-  ls
   echo "mod download"
   go get -t .
   go mod download
-  for os in "${osArray[@]}"; do
-    for arch in "${archs[@]}"; do
-      build "${os}" "${arch}"
-    done
-  done
+  build
 }
 
 main
