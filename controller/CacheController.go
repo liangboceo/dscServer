c251315de12e5b0f865fe259668d05539e7a322b
liangbo <liangbo01@cpirhzl.com>
2024-07-24 17:22:17 +0800

    fix:增加缓存service

--- controller/CacheController.go
+++ controller/CacheController.go
@@ -3,9 +3,7 @@ package controller
 import (
 	"dscserver/service"
 	"fmt"
-	"github.com/liangboceo/yuanboot/abstractions"
 	"github.com/liangboceo/yuanboot/web/actionresult"
-	"github.com/liangboceo/yuanboot/web/context"
 	"github.com/liangboceo/yuanboot/web/mvc"
 )
 
@@ -22,14 +20,12 @@ func NewCacheController(cacheService *service.CacheService) *CacheController {
 type CacheReq struct {
 	mvc.RequestBody
 	ServerName string `param:"serverName" doc:"服务名"`
-	Key        string `param:"key" doc:"缓存md5Key"`
+	KeyName    string `param:"keyName" doc:"缓存md5Key"`
 }
 
 // GetFrontCache 获取前台服务缓存
-func (controller CacheController) GetFrontCache(ctx *context.HttpContext, req *CacheReq) actionresult.IActionResult {
-	var config abstractions.IConfiguration
-	_ = ctx.RequiredServices.GetService(&config)
-	key := fmt.Sprintf("%s:%s:%s", config.GetString("yuanboot.application.name"), req.ServerName, req.Key)
+func (controller CacheController) GetFrontCache(req *CacheReq) actionresult.IActionResult {
+	key := fmt.Sprintf("%s:%s", req.ServerName, req.KeyName)
 	return actionresult.Data{
 		ContentType: "application/json; charset=utf-8",
 		Data:        []byte(controller.cache.GetCache(key)),
