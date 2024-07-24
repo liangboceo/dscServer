1e62d7ecbe03a9ebeeb0c5c1f50f0df468d9df14
liangbo <liangbo01@cpirhzl.com>
2024-07-24 17:32:17 +0800

    fix:增加缓存service

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+osArray=("linux" "darwin" "freebsd" "windows")
+archs=("amd64" "386")
+version=${1-"0.0.1-preview1"}
+out_file="dscServer"
+
+build() {
+  os=$1
+  arch=$2
+  go env -w GOPROXY=https://goproxy.cn,direct
+  GOOS=$os GOARCH=$arch go build -o "${out_file}" .
+  tgzName="${out_file}_${version}_${os}_${arch}.tar.gz"
+  rm -rf build
+  mkdir build
+  tar -czf "build/${tgzName}" "${out_file}"
+  rm -f "${out_file}"
+}
+
+main() {
+  ls
+  echo "mod download"
+  go get -t .
+  go mod download
+  for os in "${osArray[@]}"; do
+    for arch in "${archs[@]}"; do
+      build "${os}" "${arch}"
+    done
+  done
+}
+
+main
